7a2a423e51daa934c7cd0fb9d7a7122159f20843
Adithya M S <47377931+Adithya-Meda@users.noreply.github.com>
2026-09-25 22:23:35 +0530

    feat: expand fetching script to cover all 24 global regions in the frontend

--- src/scripts/fetch-aws-prices.js
+++ src/scripts/fetch-aws-prices.js
@@ -145,8 +145,17 @@ const main = async () => {
   liveBaselineCosts["AWS WAF"]["Standard"] = 5.0;
   liveBaselineCosts["AWS KMS"]["Standard"] = 1.0;
 
-  const regions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2",
-    "eu-central-1", "eu-west-1", "ap-southeast-1", "ap-northeast-1"];
+  const regions = [
+    "us-east-1", "us-east-2", "us-west-1", "us-west-2",
+    "ca-central-1", 
+    "eu-west-1", "eu-west-2", "eu-west-3", "eu-central-1", "eu-north-1", "eu-south-1",
+    "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", 
+    "ap-northeast-1", "ap-northeast-2", "ap-northeast-3",
+    "ap-south-1", "ap-south-2", "ap-east-1",
+    "sa-east-1",
+    "me-south-1", "me-central-1",
+    "af-south-1"
+  ];
     
   const dbRecords = [];
 
@@ -180,4 +189,5 @@ const main = async () => {
 };
 
 main();
+
 
